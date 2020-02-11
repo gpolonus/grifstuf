42fb592905693d3ba19cde198305165293389cab
polonusg <polonusg@hnicorp.com>
2020-02-11 15:53:13 -0600

    Added Lang attribute

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ import Document, { Head, Html, Main, NextScript } from 'next/document'
 export default class extends Document {
   render () {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
