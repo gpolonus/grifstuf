4081b029595ab3a65fb95e37d597b2a07921b7d4
polonusg <polonusg@hnicorp.com>
2020-02-11 15:56:00 -0600

    Added noopener to Lippy

--- pages/lippy.js
+++ pages/lippy.js
@@ -9,7 +9,7 @@ export default () => (
     <div className="text-contents">
       <p>Lippy was a goldfish. He was fantastic and wonderful and a big fan of the perma-frown look even though he is reported to have been a rather happy fish. I hope to share the joy that this exceptional fish has brought me by showing him to as many people as possible.</p>
       <p>
-        <a target="_blank" href="https://flickr.com/photos/bensonkua/3150903855/in/album-72157612027483317/">
+        <a target="_blank" rel="noopener" href="https://flickr.com/photos/bensonkua/3150903855/in/album-72157612027483317/">
           The original photo of Lippy
         </a>
       </p>
