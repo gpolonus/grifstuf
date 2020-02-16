71de9bef4a6e3b5b396f172429c129f3e2626bd8
polonusg <polonusg@hnicorp.com>
2020-02-16 17:39:09 -0600

    Added stuf index

---
+++ src/pages/stuf/index.jsx
@@ -0,0 +1,42 @@
+
+import React from 'react'
+
+// <style>
+
+// </style>
+// <div onkeydown="keyBooleans()" onkeyup="keyBooleans()" onmousedown="keyBooleans()" onmousemove="keyBooleans()">
+// <canvas id="canvas"></canvas>
+// <script src="canvasLibrary.js"></script>
+// <script src="colorFuns.js"></script>
+// <script>
+
+// function loop(state, actions, render, time, every) {
+//   let s = state
+//   const a = Object.entries(actions).reduce((ac, [name, action]) => ({
+//     ...ac,
+//     [name]: (data) => {
+//       s = action(data)(s)
+//     }
+//   }), {})
+
+//   return [a, () => {
+//     every = (every && a[every]) || (() => {})
+//     const fun = () => {
+//       every(s)
+//       render(s)
+//       setTimeout(fun, time)
+//     }
+//     setTimeout(fun, time)
+//   }]
+// }
+
+
+// </script>
+// </body>
+// </html>
+
+export default () => {
+  return (
+    <h1>Stuf</h1>
+  )
+}
