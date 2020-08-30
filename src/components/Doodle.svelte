2c11b0454fceb868f256b0e522a15da24ddd0470
Griffin Polonus <gpolonus@gmail.com>
2020-08-30 18:08:20 -0500

    polished single doodle page some

--- src/components/Doodle.svelte
+++ src/components/Doodle.svelte
@@ -1,22 +1,60 @@
 <script>
+  import doodles from './doodles.yaml'
+
+  export let num = 0
   let rotate = 0;
-  export let title = '';
-  export let alt = '';
-  export let date = '';
-  export let url = '';
+  $: doodle = doodles[doodles.length - 1 - num]
+  $: date = doodle.date && new Date(doodle.date).toLocaleString()
+  $: url = `/converts/output-300/${doodle.url}`
+  $: title = doodle.title
+  $: alt = doodle.alt
 </script>
 
 <style>
+  h1 {
+    text-align: center;
+  }
 
   .doodle {
-    display: inline-block;
-    padding: 10px;
     text-align: center;
+  }
+
+  .doodle img {
+    cursor: pointer;
+  }
+
+  .doodle .controls h2 {
+    display: inline-block;
+    text-decoration: underline;
     cursor: pointer;
   }
 </style>
 
+<svelte:head>
+  <title>Grif's Doodles</title>
+</svelte:head>
+
+<h1>Grif's Doodles</h1>
 <div class='doodle'>
+  <div class='controls'>
+    {#if num > 0}
+      <a href='/doodles/{num - 1}'>
+        <h2>
+          Prev
+        </h2>
+      </a>
+    {/if}
+    {#if num < doodles.length - 1}
+      <a href='/doodles/{num + 1}'>
+        <h2>
+          Next
+        </h2>
+      </a>
+    {/if}
+    <h2 on:click={() => rotate -= 90}> 
+      Rotate
+    </h2>
+  </div>
   <h3>
     {title}
   </h3>
@@ -24,10 +62,9 @@
     {date}
   </div>
   <img
-    src='/converts/output-300/{url}'
+    src={url}
     alt={alt}
     title={alt}
-    on:click={() => rotate += 90}
     style="transform: rotate({rotate}deg)"
   />
 </div>
--- src/routes/doodles/index.svelte
+++ src/routes/doodles/index.svelte
@@ -1,39 +1,7 @@
 
 <script>
-  import doodles from './doodles.yaml'
   import Doodle from '../../components/Doodle.svelte'
-
-  export let doodle = doodles[doodles.length - 1];
-
-  $: date = new Date(doodle.date).toLocaleString()
 </script>
 
-<style>
-  .doodle-list {
-    text-align: center;
-  }
+<Doodle num={0} />
 
-  .doodle {
-    display: inline-block;
-    padding: 10px;
-    text-align: center;
-  }
-
-  h1 {
-    text-align: center;
-  }
-</style>
-
-<svelte:head>
-  <title>Grif's Doodles</title>
-</svelte:head>
-
-<h1>Grif's Doodles</h1>
-<!-- <div class='doodle-list'> -->
-  <Doodle
-    title={doodle.title}
-    alt={doodle.alt}
-    url={doodle.url}
-    date={date}
-  />
-<!-- </div> -->
