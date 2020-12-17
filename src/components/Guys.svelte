6e6c0de4909dc7629cfb7ad04066ea6bbba2719b
Griffin Polonus <gpolonus@gmail.com>
2020-12-17 07:56:38 -0600

    Attempted auto add of Desert of Depiction with alt text Our bounds are hard to see sometimes

---
+++ src/components/Guys.svelte
@@ -0,0 +1,8 @@
+
+<g id="guys">
+  <slot name='0' />
+  <slot name='1' />
+  <slot name='2' />
+  <slot name='3' />
+  <slot name='4' />
+</g>
--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -37,7 +37,6 @@
     <li><a class:selected='{segment === undefined}' href='.'>home</a></li>
     <li><a class:selected='{segment === "doodles"}' href='doodles'>doodles</a></li>
     <li><a class:selected='{segment === "distractions"}' href='distractions'>distractions</a></li>
-    <li><a class:selected='{segment === "daily"}' href='daily'>daily</a></li>
     <li><a class:selected='{segment === "about"}' href='about'>about</a></li>
     <!-- <li><a class:selected='{segment === "stuf"}' href='stuf'>stuf</a></li> -->
     <!-- <li><a class:selected='{segment === "blog"}' href='blog'>blog</a></li> -->
