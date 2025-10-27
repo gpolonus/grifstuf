7e70bab4d8b057c804349b6b0fbb739995f475cc
Grif <gpolonus@gmail.com>
2025-10-27 00:12:09 -0500

    Moved pages

---
+++ _template/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  // @ts-nocheck
+
+  import Canvas from "$lib/components/Canvas.svelte";
+  import { siteTitle } from "$lib/stores";
+
+  // t is time in seconds
+  const render = (c, x, S, C, T, R) => t => {
+  }
+
+  siteTitle.set('stuf: Template')
+</script>
+
+<!-- <Canvas {render} steppable={0.01} /> -->
+<!-- <Canvas {render} stoppable={true} /> -->
+<Canvas {render} />
