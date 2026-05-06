b924c309bcfdd3e5758e40d33603aa69e7a711fe
Grif <gpolonus@gmail.com>
2026-05-06 16:35:37 -0500

    Added recipes links

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+  let { children } = $props();
+</script>
+
+<style>
+main {
+  padding: 20px;
+}
+
+footer {
+  padding: 20px;
+  color: black;
+  font-size: 0.75em;
+  background-color: white;
+}
+</style>
+
+
+<main>
+  {@render children?.()}
+</main>
+<footer>
+  © Griffin Polonus 2026. All Rights Reserved.
+</footer>
